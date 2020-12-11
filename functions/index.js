b4ed2877c8b678baf1e0a5b17be534fd14f90d79
Sean <sw92410@hotmail.com>
2020-12-11 01:45:36 -0700

    - Template code for functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,44 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+admin.initializeApp();
+
+function AuthorizationMiddleware(req, res, next) {
+  const token = Buffer.from(req.headers.authorization, "base64").toString();
+
+  admin
+    .auth()
+    .verifyIdToken(token)
+    .then((user) => {
+      res.locals.currentUser = user;
+
+      next();
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+app.get("/", AuthorizationMiddleware, async (req, res) => {
+  const currentUser = res.locals.currentUser;
+
+  const dbUser = await admin
+    .database()
+    .ref(`/users/${currentUser.uid}`)
+    .once("value");
+
+  res.send(dbUser);
 });
+
+// app.listen(8000, () => {
+//   console.log("Server started at http://localhost:8000/");
+// });
+
+exports.express = functions.https.onRequest(app);
