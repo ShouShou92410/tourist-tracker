2cc5f28b6b4119fba1394858f2bca16361fa7987
Sean <sw92410@hotmail.com>
2020-12-10 18:03:50 -0700

    - Add template files - Clean up MenuBar

---
+++ ui/src/component/content/traveller/TravellerDataEntry.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function TravellerDataEntry() {
+	return <h1>TravellerDataEntry</h1>;
+}
+
+export default TravellerDataEntry;
--- ui/src/component/root/Dashboard.js
+++ ui/src/component/root/Dashboard.js
@@ -1,12 +1,47 @@
 import React, { useContext } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import SideBar from '../content/SideBar';
 import { UserContext } from '../../utility/Context';
+import Enumeration from '../../utility/Enumeration';
+import TravellerDataEntry from '../content/traveller/TravellerDataEntry';
+import SiteOwnerDataEntry from '../content/siteOwner/SiteOwnerDataEntry';
+import TravellerRecommendation from '../content/traveller/TravellerRecommendation';
+import SiteOwnerRecommendation from '../content/siteOwner/SiteOwnerRecommendation';
 
 function Dashboard() {
 	const currentUser = useContext(UserContext);
+
+	const routeElement = () => {
+		switch (currentUser.type) {
+			case Enumeration.UserType.TRAVELLER:
+				return (
+					<Switch>
+						<Route path="/recommendation">
+							<TravellerRecommendation />
+						</Route>
+						<Route path="/">
+							<TravellerDataEntry />
+						</Route>
+					</Switch>
+				);
+			case Enumeration.UserType.SITEOWNER:
+				return (
+					<Switch>
+						<Route path="/recommendation">
+							<SiteOwnerRecommendation />
+						</Route>
+						<Route path="/">
+							<SiteOwnerDataEntry />
+						</Route>
+					</Switch>
+				);
+			default:
+				break;
+		}
+	};
 
 	return (
 		<Container>
@@ -14,9 +49,7 @@ function Dashboard() {
 				<Col>
 					<SideBar />
 				</Col>
-				<Col>
-					<h1>Welcome {currentUser?.name}</h1>
-				</Col>
+				<Col>{routeElement()}</Col>
 			</Row>
 		</Container>
 	);
