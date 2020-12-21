502a0234e2af928dc5e97bd837f39ed2f44cbfb7
Sean <sw92410@hotmail.com>
2020-12-20 22:06:04 -0700

    - Improve site owner recommendation ui

--- ui/src/component/content/siteOwner/SiteOwnerDataEntry.js
+++ ui/src/component/content/siteOwner/SiteOwnerDataEntry.js
@@ -11,6 +11,9 @@ import firebase from '../../../services/Firebase';
 function SiteOwnerDataEntry() {
 	const history = useHistory();
 	const [siteId, setSiteId] = useState(null);
+	const [recommendations, setRecommendations] = useState({
+		...Array(Object.values(Enumeration.Amenity).length).fill(null),
+	});
 	const [form, setForm] = useState({
 		name: '',
 		address: '',
@@ -28,14 +31,20 @@ function SiteOwnerDataEntry() {
 				address: site.address,
 			});
 
-			const newAmenityForm = {
-				...Array(Object.values(Enumeration.Amenity).length).fill(null),
-			};
+			const newAmenityForm = { ...amenityForm };
+			const newRecommendations = { ...recommendations };
 			site.amenities.split(',').forEach((x) => (newAmenityForm[x - 1] = x));
 			setAmenityForm(newAmenityForm);
+
+			const rec = new URLSearchParams(window.location.search);
+			const recString = rec.get('recommendations');
+			if (recString !== null) {
+				recString.split(',').forEach((x) => (newRecommendations[x - 1] = true));
+				setRecommendations(newRecommendations);
+			}
 		};
 
-		const id = window.location.href.split('/').pop();
+		const id = window.location.pathname.split('/').pop();
 		if (id !== 'dataentry') {
 			fetchData(id);
 			setSiteId(id);
@@ -107,8 +116,13 @@ function SiteOwnerDataEntry() {
 							>
 								{label}
 								{amenityForm[index] && (
-									<Badge variant="success" className="float-right">
+									<Badge variant="success" className="ml-1 mr-1 float-right">
 										Added
+									</Badge>
+								)}
+								{recommendations[index] && (
+									<Badge variant="warning" className="ml-1 mr-1 float-right">
+										Recommend
 									</Badge>
 								)}
 							</ListGroup.Item>
